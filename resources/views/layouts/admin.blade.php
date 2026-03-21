e423e3e4d8c2cf0c8b26f47a89a99d7bc43dc648
mxJosS <jangel2003.kimba4@gmail.com>
2026-03-21 02:06:49 -0600

    feat: adding dynamic links to sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,8 +11,11 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+      {{-- Font awesome --}}
+        <script src="https://kit.fontawesome.com/6244811c40.js" crossorigin="anonymous"></script>
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 
         <!-- Styles -->
         @livewireStyles
